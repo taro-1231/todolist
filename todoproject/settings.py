5799a0c662901d8e5ecd8db922e8dc466a46adf0
taro-1231 <okajimaryota1231@gmail.com>
2025-03-05 18:13:14 +0900

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = 'django-insecure-wf8k83d-ott^okp4eu@g!)qw(c@3$s$p$*vr_8tjv_#ixj0se3
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'todolist-d286.onrender.com',  # Render のデプロイURL
-    'localhost',
-    '127.0.0.1'
+    'todolist-d286.onrender.com'
 ]
 
 
